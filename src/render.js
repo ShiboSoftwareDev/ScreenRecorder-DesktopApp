0dfa4b248316f7ad306f154ffe7bc8881ca9a581
Ahmed <ahmedsheb2003@yahoo.com>
2024-02-20 19:25:34 +0200

    intial commit

---
+++ src/render.js
@@ -0,0 +1,9 @@
+const videoElement = document.querySelector("video");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const videoSelectBtn = document.getElementById("videoSelectBtn");
+videoSelectBtn.onclick = getVideoSources;
+
+async function getVideoSources() {
+  const inputSources = await window.electronAPI.videoSelectBtnClicked();
+}
